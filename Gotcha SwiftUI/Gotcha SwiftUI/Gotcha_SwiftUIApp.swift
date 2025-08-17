8cdd7a2850ef935643d05f2abfd6ca0e21913341
modrzywolek <odrzywolek.m@gmail.com>
2025-08-17 18:30:48 +0200

    setup xcode project

---
+++ Gotcha SwiftUI/Gotcha SwiftUI/Gotcha_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Gotcha_SwiftUIApp.swift
+//  Gotcha SwiftUI
+//
+//  Created by Marta Odrzywolek on 17/08/2025.
+//
+
+import SwiftUI
+
+@main
+struct Gotcha_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
